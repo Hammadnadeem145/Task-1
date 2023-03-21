2aeeb316305c291cedf8d7d9377a2d34305a4cb2
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-22 03:52:00 +0500

    tsfile-done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function TodoApp() {
   const [newTodo, setNewTodo] = useState<string>("");
